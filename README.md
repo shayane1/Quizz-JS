69e4dee99c2df0e05508ee7820bfa536d3edeba4
Shayane Antonio <shayane.antonio@fatec.sp.gov.br>
2022-07-09 18:15:47 -0300

    att

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Quizz-JS
 É um quizz com contador de tempo usando a linguagem JavaScript. 
+
+ Feito com ❤️ por Shayane Santos
---
+++ question.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Qual dessas não é uma linguagem de programação?",
+      answer: "HTML",
+      options: [
+        "JavaScript",
+        "PHP",
+        "HTML",
+        "Ruby"
+      ]
+    },
+    {
+      numb: 2,
+      question: "O que significa o comando printf na linguagem C?",
+      answer: "Escreva",
+      options: [
+        "Leia",
+        "Escreva",
+        "Calcule",
+        "Pare"
+      ]
+    },
+    {
+      numb: 3,
+      question: "O que significa #includestdio.h?",
+      answer: "Apenas uma hashtag",
+      options: [
+        "Inclusão de sistema",
+        "Apenas uma hashtag ",
+        "inclusão de comentário",
+        "Inclusão de biblioteca"
+      ]
+    },
+    {
+      numb: 4,
+      question: "Para uma nota de um aluno qual tipo de dados devo utilizar?",
+      answer: "float",
+      options: [
+        "char",
+        "float",
+        "int",
+        "boolean"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Qual é a diferença de RAM e ROM?",
+      answer: "RAM é volátil, ROM é não-volátil",
+      options: [
+        "RAM é volátil, ROM é não-volátil",
+        "RAM não é volátil, ROM é volátil",
+        "RAM armazena mais memória",
+        "Nenhuma das alternativas está correta"
+      ]
+    },
+  ];
